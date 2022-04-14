edb526acdc84aa5603c5e590724ae57f5bebe3bb
Brendow <brandon@sistemairis.com.br>
2022-04-14 09:16:10 -0300

    fix: footer

--- header/src/app/app-routing.module.ts
+++ header/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
+//
 const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
 
 @NgModule({
